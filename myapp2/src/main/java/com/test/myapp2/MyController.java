8319662325ea529acee90bfc785c39893925e377
Dennis <itgenius1004@gmail.com>
2024-08-05 14:56:41 +0900

    ok

--- myapp2/src/main/java/com/test/myapp2/MyController.java
+++ myapp2/src/main/java/com/test/myapp2/MyController.java
@@ -3,11 +3,10 @@ package com.test.myapp2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-public class MyControler {
+public class MyController {
     @Autowired
     Address address;
     @Autowired
