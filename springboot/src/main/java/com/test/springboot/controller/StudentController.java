18ea82691303d7e878cadab7f05296efb8622ef9
Dennis <itgenius1004@gmail.com>
2024-08-06 10:57:42 +0900

    ok

--- springboot/src/main/java/com/test/springboot/controller/StudentController.java
+++ springboot/src/main/java/com/test/springboot/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.springboot.entity.Student;
@@ -42,6 +43,18 @@ public class StudentController {
         @PathVariable("last-name") String lastName){
         Student student = 
         new Student(studentId, firstName, lastName);
+        return ResponseEntity.ok(student);
+    }
+
+    // http://localhost:8080/students/query?id=1&firstName=Dennis&lastName=Lee
+    @GetMapping("query")
+    public ResponseEntity<Student> studentRequestVariable(
+        @RequestParam int id,
+        @RequestParam String firstName,
+        @RequestParam String lastName
+    ){
+        Student student = 
+        new Student(id, firstName, lastName);
         return ResponseEntity.ok(student);
     }
 }
