cb405e8ac2f3a01a3655a14fc01468ec0c6151f0
Dennis <itgenius1004@gmail.com>
2024-08-06 16:03:08 +0900

    ok

--- springbootapp/src/main/java/com/test/springbootapp/controller/PostController.java
+++ springbootapp/src/main/java/com/test/springbootapp/controller/PostController.java
@@ -3,11 +3,16 @@ package com.test.springbootapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.springbootapp.dto.PostDto;
 import com.test.springbootapp.service.PostService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;;
 
 
@@ -21,5 +26,11 @@ public class PostController {
     @GetMapping    
     public List<PostDto> getAllPosts() {
         return postService.getAllPosts();
+    }
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(
+        @RequestBody PostDto postDto) {
+        return new ResponseEntity<>(
+            postService.createPost(postDto), HttpStatus.CREATED);
     }
 }
