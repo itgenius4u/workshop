220160a097d73d1a8e4bfd925bb2e5330eaa3244
Dennis <itgenius1004@gmail.com>
2024-08-06 10:39:01 +0900

    ok

--- springboot/src/main/java/com/test/springboot/controller/StudentController.java
+++ springboot/src/main/java/com/test/springboot/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.test.springboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +13,7 @@ import com.test.springboot.entity.Student;
 @RestController
 @RequestMapping("students")
 public class StudentController {
-    // http://localhost:8080/student
+    // http://localhost:8080/student/student
     @GetMapping("student")
     public ResponseEntity<Student> getStudent() {
         Student student = new Student();
@@ -20,6 +23,13 @@ public class StudentController {
         return ResponseEntity.ok()
         .header("custom-header", "Dennis").
         body(student);
+    }
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Dennis1", "Lee"));
+        students.add(new Student(1, "Dennis2", "Lee"));
+        return ResponseEntity.ok(students);
     }
 
 }
