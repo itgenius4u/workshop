08e33f77a318a82f362e1388ac2fa7105d6485db
Dennis <itgenius1004@gmail.com>
2024-08-06 11:20:29 +0900

    ok

--- springboot/src/main/java/com/test/springboot/controller/StudentController.java
+++ springboot/src/main/java/com/test/springboot/controller/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,5 +75,11 @@ public class StudentController {
             + ", " + student.getFirstName()
             + ", " + student.getLastName());
         return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable("id") int studentId) {
+        System.out.println("Deleted : " + studentId);
+        return ResponseEntity.ok("Student deleted");        
     }
 }
