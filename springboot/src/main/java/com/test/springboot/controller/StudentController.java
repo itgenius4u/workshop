50be4cdc112d95a7fd5bde6971a6b182237ca443
Dennis <itgenius1004@gmail.com>
2024-08-06 11:08:49 +0900

    ok

--- springboot/src/main/java/com/test/springboot/controller/StudentController.java
+++ springboot/src/main/java/com/test/springboot/controller/StudentController.java
@@ -3,9 +3,12 @@ package com.test.springboot.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,5 +59,20 @@ public class StudentController {
         Student student = 
         new Student(id, firstName, lastName);
         return ResponseEntity.ok(student);
+    }
+
+    // @PostMapping @RequestBody
+    // {
+    //     "id" : 1,
+    //     "firstName" : "Dennis",
+    //     "lastName" : "Lee"
+    // }
+    @PostMapping("create")
+    public ResponseEntity<Student> createStudent(
+        @RequestBody Student student){
+            System.out.println(student.getId() 
+            + ", " + student.getFirstName()
+            + ", " + student.getLastName());
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 }
