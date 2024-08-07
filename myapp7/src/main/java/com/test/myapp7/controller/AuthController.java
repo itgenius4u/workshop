9ed6ecce7faf797c33b6cffd78b25bd3cc4ee58e
Dennis <itgenius1004@gmail.com>
2024-08-07 15:52:45 +0900

    ok

---
+++ myapp7/src/main/java/com/test/myapp7/controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.test.myapp7.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+
+}
