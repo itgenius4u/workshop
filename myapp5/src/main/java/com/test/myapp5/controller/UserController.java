fb10f512e4ac0db66cb69f6ec414b2b88eef9518
Dennis <itgenius1004@gmail.com>
2024-08-07 11:25:03 +0900

    ok

--- myapp5/src/main/java/com/test/myapp5/controller/UserController.java
+++ myapp5/src/main/java/com/test/myapp5/controller/UserController.java
@@ -1,19 +1,24 @@
 package com.test.myapp5.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.myapp5.dto.UserDto;
 import com.test.myapp5.entity.User;
+import com.test.myapp5.exception.UserNotFoundException;
 import com.test.myapp5.service.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class UserController {
@@ -26,9 +31,17 @@ public class UserController {
         return new ResponseEntity<>(userService.allUser(), HttpStatus.OK);
     }
     @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@RequestBody UserDto userReq){
+    public ResponseEntity<User> registerUser(@Valid @RequestBody UserDto userReq){
         
         return new ResponseEntity<>(userService.save(userReq), HttpStatus.CREATED);
+    }
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable int id) throws UserNotFoundException {
+        Optional<User> user = userService.getUserById(id);
+        if(user.isPresent()){
+            return new ResponseEntity<>(user, HttpStatus.FOUND);
+        }
+        throw new UserNotFoundException("User Not Found");
     }
 
 }
