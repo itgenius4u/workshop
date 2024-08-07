aae3351a5edf086ae7e8e22fe23bda25e9965abd
Dennis <itgenius1004@gmail.com>
2024-08-07 15:34:32 +0900

    ok

--- myapp8/src/main/java/com/test/myapp8/SecurityConfiguration.java
+++ myapp8/src/main/java/com/test/myapp8/SecurityConfiguration.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.security.core.userdetails.User;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
@@ -14,10 +18,19 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests((authorize) -> authorize
-						.anyRequest().authenticated()
-				)
+						.anyRequest().authenticated())
 				.httpBasic(withDefaults())
 				.formLogin(withDefaults());
 		return http.build();
     }
+
+    @Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
 }
