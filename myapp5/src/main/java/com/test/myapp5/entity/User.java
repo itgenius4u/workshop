7941c7d366430bf6f645546600707649396ce673
Dennis <itgenius1004@gmail.com>
2024-08-07 10:16:10 +0900

    ok

--- myapp5/src/main/java/com/test/myapp5/entity/User.java
+++ myapp5/src/main/java/com/test/myapp5/entity/User.java
@@ -1,13 +1,17 @@
 package com.test.myapp5.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "users")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
