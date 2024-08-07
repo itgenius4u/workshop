674ca72fb55c4cdfe3c75316ef1c541b672374b7
Dennis <itgenius1004@gmail.com>
2024-08-07 11:06:40 +0900

    ok

--- myapp5/src/main/java/com/test/myapp5/controller/UserController.java
+++ myapp5/src/main/java/com/test/myapp5/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.test.myapp5.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.test.myapp5.entity.User;
 import com.test.myapp5.service.UserService;
 
 @RestController
@@ -10,5 +17,10 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return new ResponseEntity<>(userService.allUser(), HttpStatus.OK);
+    }
 
 }
