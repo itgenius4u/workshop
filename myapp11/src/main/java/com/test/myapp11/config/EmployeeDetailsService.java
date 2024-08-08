c371a6a8948ccbcd79e44cd34665c2e184bee565
Dennis <itgenius1004@gmail.com>
2024-08-08 11:36:11 +0900

    ok

---
+++ myapp11/src/main/java/com/test/myapp11/config/EmployeeDetailsService.java
@@ -0,0 +1,39 @@
+package com.test.myapp11.config;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.test.myapp11.entity.Employee;
+import com.test.myapp11.repository.EmployeeRepository;
+
+import lombok.Data;
+
+@Service
+@Data
+public class EmployeeDetailsService implements UserDetailsService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Employee employee = employeeRepository.findByUsername(username);
+
+        Set<GrantedAuthority> authorities = employee.getRoles().stream()
+                .map((roles) -> new SimpleGrantedAuthority(roles.getName()))
+                .collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(
+                username,
+                employee.getPassword(),
+                authorities
+        );
+    }
+}
