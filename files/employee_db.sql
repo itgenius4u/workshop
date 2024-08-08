632e22ba4ea5fd5115d2964c8278c9b7e162d65f
Dennis <itgenius1004@gmail.com>
2024-08-08 11:49:16 +0900

    ok

---
+++ files/employee_db.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee VALUES
+(1,'user1@gmail.com','user1','$2a$12$66f42PMT1UiRvx.9Dc7//OvVKykR6XiQ7q0reKQAiknMHLq8djub2','user1'),
+(2,'user2gmail.com','user2','$2a$12$66f42PMT1UiRvx.9Dc7//OvVKykR6XiQ7q0reKQAiknMHLq8djub2','user2');
+
+INSERT INTO roles VALUES (1,'ROLE_USER'),(2,'ROLE_ADMIN');
+
+INSERT INTO employee_roles VALUES (1,1),(2,2);
