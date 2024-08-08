ff173fb3f07a5097d321d7451f5bd3fd24b72d04
Dennis <itgenius1004@gmail.com>
2024-08-08 10:51:22 +0900

    ok

--- myapp11/src/main/java/com/test/myapp11/controller/EmployeeController.java
+++ myapp11/src/main/java/com/test/myapp11/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.test.myapp11.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,14 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     public String getAllEmployees() {
         return "getAllEmployees()";
     }
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String saveEmployees() {
         return "saveEmployees()";
     }
     @PutMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")    
     public String updateEmployees() {
         return "updateEmployees()";
     }    
