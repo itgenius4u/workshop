c5201ba212ca29ffded5af11842f36e9d4488c31
Dennis <itgenius1004@gmail.com>
2024-08-09 15:19:23 +0900

    ok

--- library-management-system/src/main/java/com/test/library_management_system/controller/UserController.java
+++ library-management-system/src/main/java/com/test/library_management_system/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class UserController {
         return userService.findAll();
     }   
     @PostMapping
-    public User addUser(User user) {
+    public User addUser(@RequestBody User user) {
         return userService.save(user);
     }
 }
