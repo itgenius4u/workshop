4fe5b5d284f38a11b2c59ca6cbe246a310bf59e0
Dennis <itgenius1004@gmail.com>
2024-08-09 14:02:34 +0900

    ok

--- library-management-system/src/main/java/com/test/library_management_system/entity/Book.java
+++ library-management-system/src/main/java/com/test/library_management_system/entity/Book.java
@@ -1,5 +1,19 @@
 package com.test.library_management_system.entity;
 
-public class Book {
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Getter
+@Setter
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private boolean borrowed;
+    @ManyToMany
+    @JoinColumn(name = "user_id")
+    private User borrowedBy;
 }
